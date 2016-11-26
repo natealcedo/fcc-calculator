b57205b5ab0cfe33d13863fa2cc00b2856a8abcc
Nate Alcedo <ndaljr@gmail.com>
2016-11-26 12:12:32 +0800

    Added used fonts to local folder, configured gulpfile to streamline process build and concatenations of css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var concatCss = require('gulp-concat-css');
 
 
-gulp.task('default', function () {
+gulp.task('default',['moveJs', 'concat-css', 'moveHTML'], function () {
 	browserSync.init({
-		server: './src'
+		server: './dist'
 	});
 	gulp.watch('./src/**/*').on('change', reload);
+});
+
+gulp.task('concat-css', ()=> {
+	return gulp.src('./src/**/*.css')
+	.pipe(concatCss('./'))
+	.pipe(gulp.dest('./dist/css/main.css'));
+});
+
+gulp.task('moveHTML', ()=>{
+	gulp.src('./src/*.html')
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('moveJs', ()=>{
+	gulp.src('./src/**/*.js')
+	.pipe(gulp.dest('./dist/'));
 });
