671aec90a057920d70d7034324323061fac30b16
Nate Alcedo <ndaljr@gmail.com>
2016-11-30 19:24:02 +0800

    Uglified Css as well as js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var concatCss = require('gulp-concat-css');
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat')
-
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var uglifyCss = require('gulp-uglifycss');
 
 gulp.task('default',['moveJs', 'concat-css', 'moveHTML'], function () {
 	browserSync.init({
@@ -15,7 +16,11 @@ gulp.task('default',['moveJs', 'concat-css', 'moveHTML'], function () {
 
 gulp.task('concat-css', ()=> {
 	return gulp.src('./src/**/*.css')
-	.pipe(concatCss('main.css'))
+	.pipe(concatCss('main.min.css'))
+	.pipe(uglifyCss({
+		'maxLineLen': 80,
+		'uglyComments': true
+	}))
 	.pipe(gulp.dest('./dist/css/'));
 });
 
@@ -26,5 +31,10 @@ gulp.task('moveHTML', ()=>{
 
 gulp.task('moveJs', ()=>{
 	gulp.src('./src/**/*.js')
-	.pipe(gulp.dest('./dist/'));
+	.pipe(babel({
+		presets: ['es2015']
+	}))
+	.pipe(uglify())
+	.pipe(concat('main.min.js'))
+	.pipe(gulp.dest('./dist/js'));
 });
